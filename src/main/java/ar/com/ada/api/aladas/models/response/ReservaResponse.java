d87c35de3ab9fd4b5f2da38308bef64e13cdb914
lumaldonado <lulimaldonado98@hotmail.com>
2021-11-04 18:09:03 -0300

    Se agrega el Email service, pasaje response y reserva response

---
+++ src/main/java/ar/com/ada/api/aladas/models/response/ReservaResponse.java
@@ -0,0 +1,12 @@
+package ar.com.ada.api.aladas.models.response;
+
+import java.util.Date;
+
+public class ReservaResponse {
+
+    public Date fechaEmision;
+    public Integer reservaId;
+    public Date fechaVencimiento;
+    public String message;
+    
+}
